81549cadd5ecce3e453af67bffe3d29714022c43
Jay Clark <jay@jayeclark.dev>
2022-01-13 14:00:28 -0400

    Fix packages
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+
+app.use(express.static('client/build'));
+app.use(cors);
+
+app.get('/accounts/all', (req, res) => {
+  res.send({
+    name: 'peter',
+    email: 'peter@mit.edu',
+    password: 'secret',
+  });
+});
+
+const port = 3000;
+app.listen(port);
+console.log(`listening on port ${port}...`);
